7e42567d02133a5bcb71f4a790bb9d2b142589ec
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-04-07 22:21:49 +0530

    Uncontrol Component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import './App.css';
 import Practice from './component/website/Practice/Practice';
 import User from './component/website/Practice/User';
 function App() {
-  function parentAlret(data){
-    alert(data);
-  }
   return (
     <div className='App'>
-      <h1>Lifting state up</h1>
-      <User alert={parentAlret}/>
+      <Practice/>
     </div>
   );
 }
--- src/component/website/Practice/Practice.js
+++ src/component/website/Practice/Practice.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React,{useRef} from 'react'
 
 const Practice = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    let inputRef=useRef(null);
+    let inputRef2=useRef(null);
+    function submitButton(event){
+        event.preventDefault();
+        console.log(inputRef.current.value);
+        console.log(inputRef2.current.value);
+    }
+    return (
+        <div>
+            <h1>UnControl Component</h1>
+            <form onSubmit={submitButton}>
+                <input ref={inputRef} type='text'></input><br></br><br></br>
+                <input ref={inputRef2} type='text'></input><br></br><br></br>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default Practice
